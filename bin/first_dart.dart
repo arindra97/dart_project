db26b8338928dc292fa918e42d8190ce7a49aa30
arindra97 <ar28297@gmail.com>
2022-06-15 10:03:42 +0700

    first commit

---
+++ bin/first_dart.dart
@@ -0,0 +1,12 @@
+import 'package:first_dart/first_dart.dart' as first_dart;
+
+void main() {
+  // declare variable name with assignment operator
+  var myName = "Muhammad Arindra";
+  // declare variable myAge without assign value
+  var myAge;
+
+  // assign variable myAge with assignment operator
+  myAge = 25;
+  print('Hello ${myName}, your age is ${myAge}');
+}

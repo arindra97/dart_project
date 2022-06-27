524e795d9bad60398925d056b0e095927080c9db
arindra97 <ar28297@gmail.com>
2022-06-27 16:06:16 +0700

    fix: learn if else

--- bin/first_dart.dart
+++ bin/first_dart.dart
@@ -25,19 +25,35 @@ void main() {
 
   // print(
   //     'Kamu lahir di ${placeOfBirth}, dan stamina kamu saat ini adalah \u2665 ${stamina}');
+  stdout.write(
+      'Aplikasi Konversi Suhu \n1. Konversi dari Celcius ke Farenheit \n2. Konversi dari Celcius ke Reamur \n3. Konversi dari Celcius ke Kelvin\nMasukkan pilihan anda (1-3): ');
+  num pil = num.parse(stdin.readLineSync()!);
+
   stdout.write('Masukkan suhu dalam Celcius : ');
   num celcius = num.parse(stdin.readLineSync()!);
 
-  var reamur;
-  var kelvin;
-  reamur = celcius * 4 / 5;
-  kelvin = celcius + 273;
-
-  print(
-      '$celcius derajat celcius sama dengan \n ${fnFarenheit(celcius)} derajat Farenheit \n$reamur derajat Reamur \n$kelvin derajat Kelvin');
+  if (pil == 1) {
+    print(
+        '\n$celcius derajat celcius sama dengan ${fnFarenheit(celcius)} derajat Farenheit');
+  } else if (pil == 2) {
+    print(
+        '\n$celcius derajat celcius sama dengan ${fnReamur(celcius)} derajat Reamur');
+  } else if (pil == 3) {
+    print(
+        '\n$celcius derajat celcius sama dengan ${fnKelvin(celcius)} derajat Kelvin');
+  } else {
+    print('\nMaaf, pilihan anda tidak ada');
+  }
 }
 
 double fnFarenheit(num suhu) {
-  num konversi_f;
-  return konversi_f = suhu * 9 / 5 + 32;
+  return suhu * 9 / 5 + 32;
+}
+
+double fnReamur(num suhu) {
+  return suhu * 4 / 5;
+}
+
+double fnKelvin(num suhu) {
+  return suhu + 273;
 }
